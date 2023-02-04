e06fbecde87158ce8a232dc78a2f29c4a822df42
Joseph Adofo Osei <86253119+theboyce@users.noreply.github.com>
2023-02-04 20:27:14 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//Listen for submit
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateResults);
+
+//Create a function to calculate results
+function calculateResults(e) {
+  console.log('Calculating...');
+
+  //ui vars
+
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your nunmbers');
+  }
+
+  e.preventDefault();
+}
